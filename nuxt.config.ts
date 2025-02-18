5b0371a54cfbccab0ab31c2cb394187b12fe9a5b
4ndef1ned <4ndefln3d@gmail.com>
2025-02-18 18:12:12 +0300

    Добавил nuxt auth utils

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ["@prisma/nuxt"],
+  modules: ["@prisma/nuxt", 'nuxt-auth-utils'],
+  runtimeConfig: {
+    oauth: {
+      'yandex': {
+        clientId: 'e0d561a721294cadb10a1fe467cb9613',
+        clientSecret: '0759017980084569ba337b2a3bb31ee5'
+      }
+    }
+  }
 })
